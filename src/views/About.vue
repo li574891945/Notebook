6290b1217a80d3e89f5adac766ba71edcdaea13a
linxiaomao <574891945@qq.com>
2019-04-16 13:39:20 +0800

    new

---
+++ src/views/About.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="about">
+        <tops></tops>
+        <div id="main" class="content">
+            <div class="container">
+                <article id="post-1202" class="js-gallery post-1202 post type-post status-publish format-standard has-post-thumbnail hentry category-uncategorized tag-11 tag-22 tag-29">
+                    <section class="post_content">
+                        <header class="post_header">
+                            <h1 class="post_title">给大家介绍一下</h1>
+                        </header>
+                        <div class="post-body js-gallery">
+                            <p>这几天被朋友圈的“男友体”刷屏了，吃瓜群众硬是要跟风才感觉自己跟得上潮流吗？然而媒体却猫在一边看着热点流量刷刷刷变现，粉丝一边被卖了还一边帮别人数钱，That so naive，who tm care ！</p>
+                            <p>现在的明星的影响力足以影响国内娱乐圈的"半壁江山"，发条微博，新浪都恐慌好几天，来来来，蹭个热点给大家介绍一下我的女朋友。</p>
+                            <p>单身狗哪有女朋友，汪！汪！哈哈，（翻白眼）</p>
+                            <p>回来，几月不正经写
+                                <a class="autotag" href="" title="【查看含有[更新]标签的文章】" target="_blank">更新</a>文，不会就这样吧啦几句就跑的，说回正事；当前 Yarn 也差不多使用了一年的时间，发售也同样一年了，起初不做任何宣传的情况下，也有几十份的出售，默默地挂着，等待下一个“有缘人”，最近被推倒了
+                                <a class="autotag" href="" title="【查看含有[主题]标签的文章】" target="_blank">主题</a>网，突然间询问的人多了起来，然而少数的是购买者，无用功的时间有点多啊，怎么说也是该感谢，因为至少被人认可是件让我觉得值得和高兴的事。更多的，我该感谢的是每一位用户和每一次用心给我反馈的用户，因为我始终认为别人的时间是金钱，别人能在这这浪费时间说明至少我的作品还是存在一定的“等价”价值，这也是每一次的版本更新我都会在邮件结尾以“感谢大家对 Yarn 的认可和支持”致谢的原因。他们说这个价格，可能不是强大的，但最好看最有个性的就是 Yarn 了， 69 元买不了吃亏，买不了上当！</p>
+                            <a href="" class="blog-card-title-link">
+                                <div class="blog-card">
+                                    <div class="blog-card-thumbnail">
+                                    </div>
+                                    <div class="blog-card-content">
+                                        <div class="blog-card-date clearfix">2016-10-19 09:46</div>
+                                        <div class="blog-card-title">关于购买Yarn</div>
+                                        <div class="blog-card-excerpt">首先说明一下，Yarn是本人第一款付费主题，用心用力，只为小众，不卖代码。函数各种代码满网络可找，设计可以千差万别。Yarn定位是清新、单栏、个性，如果你想要的是一款与众不同的主题，那么Yarn是您的</div>
+                                    </div>
+                                </div>
+                            </a>
+                            <hr>
+                            <p><span class="yellow-under">ONE MORE THING</span></p>
+                            <p>也是说到 Yarn 已经用的时间一年了，人总是向往对美好的事物，所以这次是 Dcras ！至于命名有什么含义，以后再告诉大家，聪明的您可能从旁边的预览图能找到答案。</p>
+                            <p>当然 Dcras 也是一款 wordpress 付费主题，将会应用到更多 WordPress 支持的强大功能，模版更丰富，至于大家关系的价格问题，我会说：只有 Yarn 的用户可以向我“讨价还价”！其他一律原价！这是对我 Yarn 用户承诺最真诚的福利。</p>
+                        </div>
+                        <div class="meta split split--responsive cf">
+                            <div class="split__title">
+                                <time datetime="2017-10-02 01:44">2017/10/02</time>
+                                <!--<span class=""><a href="" rel="tag">主题</a><a href="" rel="tag">日常</a><a href="" rel="tag">更新</a> </span>-->
+                            </div>
+                            <!--<div id="social-share"><span class="entypo-share"><i class="iconfont"></i>分享</span></div>-->
+                            <!--<div class="slide">-->
+                                <!--<a class="btn-slide" title="switch down"><i class="iconfont"></i>折叠评论区</a>-->
+                            <!--</div>-->
+                        </div>
+                    </section>
+                </article>
+            </div>
+
+        </div>
+        <bottoms></bottoms>
+    </div>
+</template>
+<script>
+    import tops from '../components/tops.vue'
+    import bottoms from '../components/bottoms.vue'
+    // @ is an alias to /src
+    export default {
+        name: 'home',
+        components: {
+            tops,
+            bottoms,
+        }
+    }
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="home">
+        <tops></tops>
+        <div id="main" class="content">
+            <bookList></bookList>
+        </div>
+        <bottoms></bottoms>
+    </div>
+</template>
+<style>
+    @import "../assets/css/index.css";
+ </style>
+<script>
+    import tops from '../components/tops.vue'
+    import bottoms from '../components/bottoms.vue'
+    import bookList from '../components/bookList.vue'
+    // @ is an alias to /src
+    export default {
+        name: 'home',
+        components: {
+            tops,
+            bottoms,
+            bookList
+        }
+    }
+</script>
